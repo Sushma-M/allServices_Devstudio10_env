2ebd9414acbd60a04439896c834ec23f6ae97da5
Sushma Vcsuser <sushma.vcsuser@wavemaker.com>
2018-11-09 06:30:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "clear-glass",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision",
+  "homePage" : "Main",
+  "name" : "SalesVision",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
